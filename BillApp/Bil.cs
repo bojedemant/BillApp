ed038c8cb39f202f4d1774ade8d33974e231d0f6
boje <bd@teas.dk>
2017-11-27 10:07:29 +0100

    Bilap

---
+++ BillApp/Bil.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace BillApp
+{
+    public class Bil
+    {
+        public int BilId { get; set; }
+
+        public string Model { get; set; }
+
+        //public int AntalCylindre { get; set; }  //nej  .. brug propfull se herunder
+        private int antalCylindre;
+
+        public int AntalCylindre
+        {
+            get { return antalCylindre; }
+            set {
+                antalCylindre = value;
+                if (value < 4 || value > 8)
+                //    value = 4;   //en muligehed ...'
+                    throw new ApplicationException("Forkert antal cylinbdre"); // en anden mulighed
+                    
+            }
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("Bil : " + this.Model);
+        }
+
+        public Bil()
+        {
+
+        }
+
+        public Bil(int model)
+        {
+
+        }
+
+
+
+
+    }
+
+    public class PersonBil : Bil
+    {
+        public int AntalBarneSæder { get; set; }
+
+        public override void Print()
+        {
+            Console.WriteLine("Persombil : " + this.Model);
+        }
+    }
+
+    public class LastBil : Bil
+    {
+        public int LasteEvne { get; set; }
+    }
+
+
+
+}
+
+
--- BillApp/Program.cs
+++ BillApp/Program.cs
@@ -10,6 +10,32 @@ namespace BillApp
     {
         static void Main(string[] args)
         {
+            Bil b = new Bil();
+            b.BilId = 1;
+            b.Model = "dyt";
+            b.AntalCylindre = 40;
+
+
+            LastBil l = new LastBil() { BilId = 2, Model = "Last", AntalCylindre = 4 };
+
+            b.Print();
+            l.Print();
+
+            PersonBil p = new PersonBil() { Model = "Z" };
+            p.Print();
+
+            Bil[] biler = new Bil[4];
+            biler[0] = b;
+            biler[1] = l;
+            biler[2] = p;
+            biler[3] = new Bil(4) ;
+            foreach (var bil in biler)
+                bil.Print();
+
+
+
         }
+
     }
 }
+
